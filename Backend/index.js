856fd3e6c9d984e9080279e4d7da54042b9c5fbd
Raghav <agarwalraghav892@gmail.com>
2023-04-11 16:13:31 +0530

    Added Payment Gateway

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,6 @@ app.use(Express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
